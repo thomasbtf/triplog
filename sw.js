3efc9abca659101d4787ea0fcd4b8bddd66837b1
Thomas Battenfeld <thomas.battenfeld@dbschenker.com>
2026-03-28 21:33:53 +0100

    feat: update service worker cache version and improve fetch handling

--- sw.js
+++ sw.js
@@ -1,13 +1,22 @@
-const CACHE = 'trips-v1';
+const CACHE = 'trips-v2';
 
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE).then(c => c.addAll(['./', './index.html', './manifest.json']))
   );
 });
 
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
